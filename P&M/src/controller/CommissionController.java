d9f4c46c2be49dcaae515d8473bf65cb55a6ce1e
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-02-03 21:57:43 -0500

    Modify order functionality now finished, testing pending

--- P&M/src/controller/CommissionController.java
+++ P&M/src/controller/CommissionController.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package controller;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import model.Commission;
@@ -13,13 +15,38 @@ import model.Commission;
  * @author mily
  */
 public interface CommissionController {
-    default public String createComission(boolean status, int furniture, int quantity, String branch){
-        Commission commission = new Commission(status,furniture,quantity,branch);
+
+    default public String createComission(boolean status, int furniture, int quantity, String branch) {
+        Commission commission = new Commission(status, furniture, quantity, branch);
         return commission.store(Controller.database);
     }
-    
-    default public List<Map<String, String>> searchCommissions (boolean completed) {
+
+    default public List<Map<String, String>> searchCommissions(boolean completed) {
         Commission commission = new Commission(true, 0, 0, "");
         return commission.listCommissions(Controller.database, completed);
+    }
+
+    default public Map<String, String> getCommissionInfo(String order_id) {
+        Commission commission = new Commission(true, 0, 0, "");
+        Map<String, String> commissionInfo = null;
+        if (commission.search(Controller.database, order_id)) {
+            return commissionInfo;
+
+        } else {
+            commissionInfo = new HashMap<>();
+            commissionInfo.put("order_id", commission.getId_());
+            commissionInfo.put("status", Boolean.toString(commission.getStatus_()));
+            commissionInfo.put("quantity", Integer.toString(commission.getQuantity_()));
+            commissionInfo.put("furniture", Integer.toString(commission.getFurniture_()));
+            commissionInfo.put("branch", commission.getBranch_());
+            return commissionInfo;
+
+        }
+    }
+
+    public default String updateCommission(String order_id, boolean status) {
+        Commission commission = new Commission(status, 0, 0, "");
+        commission.setId_(order_id);
+        return commission.update(Controller.database);
     }
 }

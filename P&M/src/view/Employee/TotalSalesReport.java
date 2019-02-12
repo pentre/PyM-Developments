63109e722f778299504c5674c6ecb0e3d34b0f46
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-02-12 13:34:34 -0500

    Made some corrections, deleted main method

--- P&M/src/view/Employee/TotalSalesReport.java
+++ P&M/src/view/Employee/TotalSalesReport.java
@@ -19,7 +19,7 @@ import org.knowm.xchart.SwingWrapper;
  *
  * @author Luis
  */
-public class TotalSalesReport extends javax.swing.JFrame implements Runnable {
+public class TotalSalesReport extends javax.swing.JFrame{
 
     Controller controller_;
 
@@ -143,42 +143,6 @@ public class TotalSalesReport extends javax.swing.JFrame implements Runnable {
 
     }//GEN-LAST:event_ShowReportButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TotalSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TotalSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TotalSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TotalSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TotalSalesReport(new Controller()).setVisible(true);
-            }
-        });
-    }
-
     private CategoryChart getChart(Map<String, List> data) {
         List<String> branches = data.get("branches");
         List<Integer> values = data.get("values");
@@ -200,8 +164,4 @@ public class TotalSalesReport extends javax.swing.JFrame implements Runnable {
     private javax.swing.JButton ShowReportButton;
     // End of variables declaration//GEN-END:variables
 
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
